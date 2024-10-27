25f3de1873ce56a24156642ce741a853f1ba17ed
Ashutosh45-maker <ashutoshparadkar05@gmail.com>
2024-10-27 20:58:10 +0530

    created tuple

---
+++ main.py
@@ -0,0 +1,17 @@
+#activity 1
+# mytpl=("hello",1,67,"world",["think","round"],True)
+
+mytuple=(21,31,42,64,1000,75489,1)
+print(mytuple[0])
+print(mytuple[-1])
+
+
+mytp=("Shop",61,"hello","level",["tuple",True,47,"nested"])
+print(mytp[4][3])
+
+
+print(mytuple[1:4])
+
+mytup=("Ashutosh","Virat","arya","Sachin","Vivian")
+for i in (mytup):
+    print("hello,nice to meet you!!",i)
