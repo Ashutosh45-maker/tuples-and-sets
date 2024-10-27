37eda23114a2e326dae41b723a9cc046f70d436b
Ashutosh45-maker <ashutoshparadkar05@gmail.com>
2024-10-27 21:18:28 +0530

    added sets

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 #activity 1
 # mytpl=("hello",1,67,"world",["think","round"],True)
 
-mytuple=(21,31,42,64,1000,75489,1)
-print(mytuple[0])
-print(mytuple[-1])
+# mytuple=(21,31,42,64,1000,75489,1)
+# print(mytuple[0])
+# print(mytuple[-1])
 
 
-mytp=("Shop",61,"hello","level",["tuple",True,47,"nested"])
-print(mytp[4][3])
+# mytp=("Shop",61,"hello","level",["tuple",True,47,"nested"])
+# print(mytp[4][3])
 
 
-print(mytuple[1:4])
+# print(mytuple[1:4])
 
-mytup=("Ashutosh","Virat","arya","Sachin","Vivian")
-for i in (mytup):
-    print("hello,nice to meet you!!",i)
+# mytup=("Ashutosh","Virat","arya","Sachin","Vivian")
+# for i in (mytup):
+#     print("hello,nice to meet you!!",i)
+
+
+
+
+
+#activity 2
+
+
+# myset={1,0,9,7,8,8,9,0,2,4,4,6,1,2,5,7,6}
+
+# myset.add(333412679)
+# print(myset)
+
+# set1={1,4,3,6,7,4,8,3,9}
+# set2={1,4,3,6,7,4,8,566,41,76}
+
+# print(set1.symmetric_difference(set2))
+# print(set2.difference(set1))
+
+
+set1={"red","blue","green","violet","orange"}
+set2={"yellow","blue","red","indigo"}
+print(set1.union(set2))
+print(set1.intersection(set2))
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+set1={1,4,3,6,7,4,8,3,9}
+# set2={1,4,3,6,7,4,8,566,41,76}
+
+# print(set1.symmetric_difference(set2))
+# print(set2.difference(set1))
